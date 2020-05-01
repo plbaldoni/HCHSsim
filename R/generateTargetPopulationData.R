db987c5b35eb4a7ac9d6e005710d8ee5ef75f3b5
Pedro Baldoni <pedrobaldoni@gmail.com>
2020-05-01 17:43:58 -0400

    Add figure

--- R/generateTargetPopulationData.R
+++ R/generateTargetPopulationData.R
@@ -1,7 +1,7 @@
 ### Generate data for TargetPopulation.RData ###
 
 ### Loading libraries
-lapply(c('data.table','mvtnorm','magrittr','survey','dplyr','tidyr','ggplot2','ggpubr','tidyverse','kableExtra'),library,character.only = T)
+lapply(c('data.table','mvtnorm','magrittr','survey','dplyr','tidyr','ggplot2','ggpubr','tidyverse','kableExtra','ggjoy'),library,character.only = T)
 # lapply(c('haven','mvtnorm','survey','magrittr',,,'ggpubr'), library, character.only = TRUE)
 
 ### Expit function
@@ -22,103 +22,103 @@ pop = pop[v.num==1,]
 
 ### sim_sexbkg: simulate multinomial distribution of sex*background ###
 sim_sexbkg = function(dat,p,sex=c('F','M'),bkg=c('D','PR','O')){
-    tb =  expand.grid(sex,bkg);colnames(tb) = c('sex','bkg')
-    tb$idx = 1:nrow(tb)
-    tb$prob = p
-    
-    dat$idx = apply(rmultinom(nrow(dat),size=1,prob=tb$prob),2,which.max)
-    dat = merge(dat,subset(tb,select=-prob),by='idx',all.x=T)
-    dat = dat[order(dat$strat,dat$BGid,dat$hhid,dat$subid),]
-    return(dat)
+  tb =  expand.grid(sex,bkg);colnames(tb) = c('sex','bkg')
+  tb$idx = 1:nrow(tb)
+  tb$prob = p
+  
+  dat$idx = apply(rmultinom(nrow(dat),size=1,prob=tb$prob),2,which.max)
+  dat = merge(dat,subset(tb,select=-prob),by='idx',all.x=T)
+  dat = dat[order(dat$strat,dat$BGid,dat$hhid,dat$subid),]
+  return(dat)
 }
 
 ### sim_covar: simulate covariates ###
 sim_covar = function(dat,ranges,means,covs,dat.par,count.seed=1,
                      covars=c('AGE','BMI','LN_NA_AVG','LN_K_AVG','LN_KCAL_AVG','LN_PROTEIN_AVG')){
-    dat <- as.data.frame(dat)
-    ncovars = length(covars)
-    df = data.frame()
-    groups = names(table(dat$idx))
-    namevar = c('age','bmi','ln_na_avg','ln_k_avg','ln_kcal_avg','ln_protein_avg')
+  dat <- as.data.frame(dat)
+  ncovars = length(covars)
+  df = data.frame()
+  groups = names(table(dat$idx))
+  namevar = c('age','bmi','ln_na_avg','ln_k_avg','ln_kcal_avg','ln_protein_avg')
+  
+  m = list()
+  v = list()
+  
+  # F=Female, M=Male
+  # D= Dominican, PR=Puerto Rican, O=other
+  m[[1]] = as.numeric(means[1,]) #F,D
+  m[[2]] = as.numeric(means[2,]) #M,D
+  m[[3]] = as.numeric(means[3,]) #F,PR
+  m[[4]] = as.numeric(means[4,]) #M,PR
+  m[[5]] = as.numeric(means[5,]) #F,O
+  m[[6]] = as.numeric(means[6,]) #M,O
+  
+  v[[1]] = matrix(as.numeric(covs[1,]),ncovars,ncovars,byrow = T) #F,D
+  v[[2]] = matrix(as.numeric(covs[2,]),ncovars,ncovars,byrow = T) #M,D
+  v[[3]] = matrix(as.numeric(covs[3,]),ncovars,ncovars,byrow = T) #F,PR
+  v[[4]] = matrix(as.numeric(covs[4,]),ncovars,ncovars,byrow = T) #M,PR
+  v[[5]] = matrix(as.numeric(covs[5,]),ncovars,ncovars,byrow = T) #F,O
+  v[[6]] = matrix(as.numeric(covs[6,]),ncovars,ncovars,byrow = T) #M,O
+  
+  for(i in groups){cat('Group: ',i,".\n")
+    subdat = subset(dat,idx==i)
     
-    m = list()
-    v = list()
+    m.topass = m[[as.numeric(i)]]
+    v.topass = v[[as.numeric(i)]]
     
-    # F=Female, M=Male
-    # D= Dominican, PR=Puerto Rican, O=other
-    m[[1]] = as.numeric(means[1,]) #F,D
-    m[[2]] = as.numeric(means[2,]) #M,D
-    m[[3]] = as.numeric(means[3,]) #F,PR
-    m[[4]] = as.numeric(means[4,]) #M,PR
-    m[[5]] = as.numeric(means[5,]) #F,O
-    m[[6]] = as.numeric(means[6,]) #M,O
+    ### Simulating Age, BMI, and log-transformed nutrients
+    subdat = cbind(subdat,rmvnorm(nrow(subdat),mean=m.topass,sigma=v.topass))
+    colnames(subdat) = c(colnames(dat),namevar)
     
-    v[[1]] = matrix(as.numeric(covs[1,]),ncovars,ncovars,byrow = T) #F,D
-    v[[2]] = matrix(as.numeric(covs[2,]),ncovars,ncovars,byrow = T) #M,D
-    v[[3]] = matrix(as.numeric(covs[3,]),ncovars,ncovars,byrow = T) #F,PR
-    v[[4]] = matrix(as.numeric(covs[4,]),ncovars,ncovars,byrow = T) #M,PR
-    v[[5]] = matrix(as.numeric(covs[5,]),ncovars,ncovars,byrow = T) #F,O
-    v[[6]] = matrix(as.numeric(covs[6,]),ncovars,ncovars,byrow = T) #M,O
-    
-    for(i in groups){cat('Group: ',i,".\n")
-        subdat = subset(dat,idx==i)
-        
-        m.topass = m[[as.numeric(i)]]
-        v.topass = v[[as.numeric(i)]]
-        
-        ### Simulating Age, BMI, and log-transformed nutrients
-        subdat = cbind(subdat,rmvnorm(nrow(subdat),mean=m.topass,sigma=v.topass))
-        colnames(subdat) = c(colnames(dat),namevar)
-        
-        idx.age = with(subdat,which(!between(age,ranges[1,1],ranges[1,2]) |
-                                        !between(bmi,ranges[2,1],ranges[2,2]) |
-                                        !between(ln_na_avg,ranges[3,1],ranges[3,2]) |
-                                        !between(ln_k_avg,ranges[4,1],ranges[4,2]) |
-                                        !between(ln_kcal_avg,ranges[5,1],ranges[5,2]) |
-                                        !between(ln_protein_avg,ranges[6,1],ranges[6,2])))
-        ### Checking for values out of the range in the population data
-        for(j in idx.age){cat(count.seed,"\r")
-            flag = T
-            while(flag){
-                subdat[j,namevar] <- rmvnorm(1,mean=m.topass,sigma=v.topass)
-                count.seed = count.seed + 1
-                flag = with(subdat,!between(age[j],ranges[1,1],ranges[1,2]) |
-                                    !between(bmi[j],ranges[2,1],ranges[2,2]) |
-                                    !between(ln_na_avg[j],ranges[3,1],ranges[3,2]) |
-                                    !between(ln_k_avg[j],ranges[4,1],ranges[4,2]) |
-                                    !between(ln_kcal_avg[j],ranges[5,1],ranges[5,2]) |
-                                    !between(ln_protein_avg[j],ranges[6,1],ranges[6,2]))
-            }
-        }
-        ### Now, simulating binary variables ###
-        subdat.par = subset(dat.par,idx==i & MODEL =='USBORN',select=c(idx,Variable,Estimate))
-        subdat.par = subdat.par[match(c("Intercept","AGE","BMI","LN_NA_AVG","LN_K_AVG","LN_KCAL_AVG","LN_PROTEIN_AVG"),subdat.par$Variable),]
-        p.par = expit(as.matrix(cbind('Intercept'=1,subdat[,namevar]))%*%subdat.par$Estimate)
-        subdat$usborn = as.numeric(1*(runif(nrow(p.par))<=p.par))
-        
-        subdat.par = subset(dat.par,idx==i & MODEL =='CHOL',select=c(idx,Variable,Estimate))
-        subdat.par = subdat.par[match(c("Intercept","AGE","BMI","LN_NA_AVG","LN_K_AVG","LN_KCAL_AVG","LN_PROTEIN_AVG","US_BORN"),subdat.par$Variable),]
-        p.par = expit(as.matrix(cbind('Intercept'=1,subdat[,c(namevar,'usborn')]))%*%subdat.par$Estimate)
-        subdat$high_chol = as.numeric(1*(runif(nrow(p.par))<=p.par))
-        
-        ### Saving data
-        df = rbind(df,subdat)
+    idx.age = with(subdat,which(!between(age,ranges[1,1],ranges[1,2]) |
+                                  !between(bmi,ranges[2,1],ranges[2,2]) |
+                                  !between(ln_na_avg,ranges[3,1],ranges[3,2]) |
+                                  !between(ln_k_avg,ranges[4,1],ranges[4,2]) |
+                                  !between(ln_kcal_avg,ranges[5,1],ranges[5,2]) |
+                                  !between(ln_protein_avg,ranges[6,1],ranges[6,2])))
+    ### Checking for values out of the range in the population data
+    for(j in idx.age){cat(count.seed,"\r")
+      flag = T
+      while(flag){
+        subdat[j,namevar] <- rmvnorm(1,mean=m.topass,sigma=v.topass)
+        count.seed = count.seed + 1
+        flag = with(subdat,!between(age[j],ranges[1,1],ranges[1,2]) |
+                      !between(bmi[j],ranges[2,1],ranges[2,2]) |
+                      !between(ln_na_avg[j],ranges[3,1],ranges[3,2]) |
+                      !between(ln_k_avg[j],ranges[4,1],ranges[4,2]) |
+                      !between(ln_kcal_avg[j],ranges[5,1],ranges[5,2]) |
+                      !between(ln_protein_avg[j],ranges[6,1],ranges[6,2]))
+      }
     }
-    df1 = df[order(df$strat,df$BGid,df$hhid,df$subid),];rm(df)
-    df1$age.strat = (df1$age>=45)
-    df1$v.num=1
+    ### Now, simulating binary variables ###
+    subdat.par = subset(dat.par,idx==i & MODEL =='USBORN',select=c(idx,Variable,Estimate))
+    subdat.par = subdat.par[match(c("Intercept","AGE","BMI","LN_NA_AVG","LN_K_AVG","LN_KCAL_AVG","LN_PROTEIN_AVG"),subdat.par$Variable),]
+    p.par = expit(as.matrix(cbind('Intercept'=1,subdat[,namevar]))%*%subdat.par$Estimate)
+    subdat$usborn = as.numeric(1*(runif(nrow(p.par))<=p.par))
     
-    #df2 = df1
-    #df2$v.num = 2
+    subdat.par = subset(dat.par,idx==i & MODEL =='CHOL',select=c(idx,Variable,Estimate))
+    subdat.par = subdat.par[match(c("Intercept","AGE","BMI","LN_NA_AVG","LN_K_AVG","LN_KCAL_AVG","LN_PROTEIN_AVG","US_BORN"),subdat.par$Variable),]
+    p.par = expit(as.matrix(cbind('Intercept'=1,subdat[,c(namevar,'usborn')]))%*%subdat.par$Estimate)
+    subdat$high_chol = as.numeric(1*(runif(nrow(p.par))<=p.par))
     
-    #df = rbind(df1,df2)
-    df = df1
-    df = df[order(df$strat,df$BGid,df$hhid,df$subid,df$v.num),]
-    
-    cat('Done!',"\n")
-    df = as.data.table(df)
-    
-    return(list('Data'=df,'HCHS.Mean'=m,'HCHS.Cov'=v))
+    ### Saving data
+    df = rbind(df,subdat)
+  }
+  df1 = df[order(df$strat,df$BGid,df$hhid,df$subid),];rm(df)
+  df1$age.strat = (df1$age>=45)
+  df1$v.num=1
+  
+  #df2 = df1
+  #df2$v.num = 2
+  
+  #df = rbind(df1,df2)
+  df = df1
+  df = df[order(df$strat,df$BGid,df$hhid,df$subid,df$v.num),]
+  
+  cat('Done!',"\n")
+  df = as.data.table(df)
+  
+  return(list('Data'=df,'HCHS.Mean'=m,'HCHS.Cov'=v))
 }
 
 ls.pop = sim_sexbkg(dat=pop,p=c(0.1953,0.1300,0.2065,0.1984,0.1421,0.1277))
@@ -255,8 +255,8 @@ save(pop,file=paste0('./RData/TargetPopulationData.RData'),compress = 'xz')
 ############# Creating Plots ##############
 ###########################################
 
-pt.title = 12
-pt.text = 12
+pt.title = 10
+pt.text = 10
 
 sdnutr = round(apply(pop[pop$v.num==1,c('ln_na_bio1','ln_na_avg','ln_na_true')],2,sd),2)
 pop.na <- as_tibble(pop[pop$v.num==1,c('ln_na_true','ln_na_bio1','ln_na_avg')]) %>%
@@ -329,34 +329,52 @@ pop.na <- as_tibble(pop[pop$v.num==1,c('ln_na_true','ln_na_bio1','ln_na_avg')]) 
   mutate(Type = recode(Type,ln_na_true = 'True', ln_na_bio1 = 'Biomarker', ln_na_avg='Self-reported'))
 pop.na$Type %<>% factor(levels = c('True','Biomarker','Self-reported'))
 
-naplot = ggplot(pop.na,aes(Sodium,color=Type,fill=Type))+
-  geom_density(alpha=0.15)+
+naplot = ggplot(pop.na,aes(x=Sodium,linetype = Type))+
+  stat_density(geom="line", position="identity")+
   theme_bw()+ylab('Density')+xlab('Log-Sodium')+
-  scale_fill_brewer(palette = 'Set1',labels=as.character(apply(cbind(c('True','Biomarker','Self-reported'),sdnutr),1,FUN = function(x){paste0(x[1],' (SD=',x[2],')')})))+
-  scale_color_brewer(palette = 'Set1',labels=as.character(apply(cbind(c('True','Biomarker','Self-reported'),sdnutr),1,FUN = function(x){paste0(x[1],' (SD=',x[2],')')})))+
-  theme(legend.title=element_blank(),legend.position = c(0.2,0.85),legend.text = element_text(size=pt.title),legend.background =  element_rect(fill = "transparent", colour = "transparent"))+
-  guides(color = guide_legend(override.aes = list(size = rel(0.3))))+ylim(0,1)
+  theme_bw() + 
+  scale_linetype_manual(values = c('solid','longdash','dotted'),
+                        labels=as.character(apply(cbind(c('True','Biomarker','Self-reported'),sdnutr),1,FUN = function(x){paste0(x[1],' (SD=',x[2],')')})))+
+  guides(linetype = guide_legend(override.aes = list(size = rel(0.3))))+ylim(0,1)+
+  theme(legend.title=element_blank(),legend.position = c(0.8,0.85),legend.text = element_text(size=pt.title),legend.background =  element_rect(fill = "transparent", colour = "transparent"))
 
 dt1 <- melt(pop[pop$v.num==1,c('subid','sex','bkg','age','bmi','ln_na_true','ln_na_bio1','ln_na_avg')],
             id.vars = c('subid','sex','bkg'),measure.vars = c('ln_na_true','ln_na_bio1','ln_na_avg'),variable.name = 'Nutrient',value.name = 'Value')
 dt1$sex %<>% plyr::mapvalues(from = c('M','F'),to = c('Male','Female')) %<>% factor(levels = c('Female','Male'))
 dt1$bkg %<>% plyr::mapvalues(from = c('D','PR','O'),to = c('Dominican','Puerto Rican','Other')) %<>% factor(levels = c('Dominican','Puerto Rican','Other'))
-dt1$Nutrient %<>% plyr::mapvalues(from = c('ln_na_true','ln_na_bio1','ln_na_avg'),to = c('True','Biomarker','Self-reported')) %<>% factor(levels = c('True','Biomarker','Self-reported'))
+dt1$Nutrient %<>% plyr::mapvalues(from = c('ln_na_true','ln_na_bio1','ln_na_avg'),to = c('True','Biomarker','Self-\nreported')) %<>% factor(levels = c('True','Biomarker','Self-\nreported'))
 
-fig1 <- ggplot(data = dt1,aes(x = sex,y = Value))+
-  facet_grid(cols = vars(bkg))+
-  geom_boxplot(aes(fill = Nutrient)) +
+fig1 <-  ggplot(dt1,aes(y = Value,x = Nutrient))+
+  facet_grid(cols = vars(bkg),rows = vars(sex))+
+  geom_boxplot(outlier.alpha = 0.1) +
+  # scale_fill_manual(values = RColorBrewer::brewer.pal(9,'Greys')[c(4,6)]) +
+  theme_bw() +
+  theme(axis.title.x = element_blank(),legend.title = element_blank(),
+        legend.position = 'bottom',legend.direction = 'horizontal',
+        legend.key = element_rect(fill = "transparent", colour = "transparent"),
+        legend.text = element_text(size=pt.title),axis.text.x = element_text(angle = 30, hjust = 1))+
+  ylab('Log-Sodium') + xlab('Nutrient')
+
+fig2 <- ggplot(merge(dt1,pop[pop$v.num==1,c('subid','sbp')],by = 'subid',all.x = T))+
+  facet_grid(cols = vars(bkg),rows = vars(sex))+
+  geom_smooth(method = lm,aes(x = Value,y = sbp,linetype = Nutrient),color = 'black',se = T,size = 0.5) +
+  scale_linetype_manual(values = c('solid','longdash','dotted')) +
   theme_bw()+
-  scale_fill_brewer(palette = 'Set1') +
-  ylab('Log-Sodium') + xlab('Sex')+
-  theme(legend.position = 'none')
+  guides(linetype = guide_legend(override.aes = list(size = rel(0.3))))+
+  xlab('Log-Sodium')+ylab('Systolic Blood Pressure')+
+  theme(legend.title=element_blank(),legend.position = 'none')
 
+
+fig.bottom <- ggarrange(fig1+theme(axis.text = element_text(size = pt.text),axis.title = element_text(size = pt.title)),
+                        fig2+theme(axis.text = element_text(size = pt.text),axis.title = element_text(size = pt.title)),
+                        ncol = 2,nrow = 1,labels = list('B','C'))
+  
 # Organizing plot
 if(!dir.exists('./Output')){system('mkdir Output')}
-pdf('./Output/Figure1_SodiumOnly.pdf',width = 7,height = 7)
+pdf('./Output/Figure1_SodiumOnly.pdf',width = 9,height = 8)
 ggarrange(naplot+theme(axis.text = element_text(size = pt.text),axis.title = element_text(size = pt.title)),
-          fig1,
-          nrow = 2,ncol = 1,labels = list('A','B'))
+          fig.bottom,
+          ncol = 1,nrow = 2,labels = list('A'),heights = c(0.35,0.65))
 dev.off()
 
 
@@ -385,9 +403,9 @@ sink('./Output/Table1.tex')
 kable(dt,booktab = T,caption = '\\label{svysamp}Characteristics of the simulated target population',
       col.names=c('','(N)','(N)','(N)','','','(N)','(\\%)'),escape=F,format = "latex",
       align = c('c','c','c','c','l','l','r','r')) %>%
-    add_header_above(c('Stratum'=1,'Block Groups'=1,'Households'=1,'Household Type'=1,'Background'=1,'Sex'=1,'Individuals'=1,'45+ years old'=1), line = F) %>%
-    collapse_rows(columns = 1:5,latex_hline = 'major',valign = 'top') %>%
-    row_spec(0,align = 'c') %>%
-    kable_styling(latex_options = "scale_down")
+  add_header_above(c('Stratum'=1,'Block Groups'=1,'Households'=1,'Household Type'=1,'Background'=1,'Sex'=1,'Individuals'=1,'45+ years old'=1), line = F) %>%
+  collapse_rows(columns = 1:5,latex_hline = 'major',valign = 'top') %>%
+  row_spec(0,align = 'c') %>%
+  kable_styling(latex_options = "scale_down")
 sink()
 
